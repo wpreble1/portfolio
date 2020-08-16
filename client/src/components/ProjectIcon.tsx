6b8c07ac309daf8bb42e73949752d871903ebe74
Will Preble <wpreble1@gmail.com>
2020-08-15 22:18:21 -0700

    Add link to project image

--- client/src/components/ProjectIcon.tsx
+++ client/src/components/ProjectIcon.tsx
@@ -1,20 +1,19 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
-interface Project {
-  title: string;
-  src: string;
-  alt: string;
-}
+import { Project } from './types';
 
 interface ProjectIconProps {
   project: Project;
 }
 
 const ProjectIcon: FC<ProjectIconProps> = ({ project }) => {
-  const { title, src, alt } = project;
+  const { title, src, alt, url } = project;
   return (
     <div className="p-4">
-      <img className="min-w-0 min-h-0" src={src} alt={alt} width="300px" />
+      <Link to={`/${url}`}>
+        <img className="min-w-0 min-h-0" src={src} alt={alt} width="300px" />
+      </Link>
       <p className="pt-2 uppercase font-mono hover:font-bold text-sm">{title}</p>
     </div>
   );
