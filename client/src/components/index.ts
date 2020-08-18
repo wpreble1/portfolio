65c136e049c44170860f51cc3991fbd433fa22de
Will Preble <wpreble1@gmail.com>
2020-08-17 21:16:33 -0700

    Add SlideDrawer to index

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Nav } from './Nav';
 export { default as ProjectIcon } from './ProjectIcon';
 export { default as ProjectList } from './ProjectList';
+export { default as SlideDrawer } from './SlideDrawer';
