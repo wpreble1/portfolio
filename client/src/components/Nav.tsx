a75b751d117e4f3bb8af7940aaa4fa46d737be88
Will Preble <wpreble1@gmail.com>
2020-08-17 21:16:55 -0700

    Convert link images to svgs

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,37 +1,97 @@
 import React, { FC, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
-
-// import pdf from '../resume.pdf';
+import {
+  LinkedIn, Github, DevTo, CodeWars,
+} from '../logos';
 
 interface NavProps {
+  toggleSlideDrawer: any;
 }
 
-const Nav: FC<NavProps> = (): ReactElement => {
+const Nav: FC<NavProps> = ({ toggleSlideDrawer }): ReactElement => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-blue-100 p-6">
-      <div className="flex items-center flex-shrink-0 mr-6">
-        {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
-        <span className="font-semibold text-xl tracking-tight uppercase font-mono"><Link to="/">Will Preble</Link></span>
+    <nav className="flex items-center justify-between bg-blue-100 px-2 py-4">
+      <div className="flex items-center flex-shrink-0 mx-6">
+        <span className="font-semibold text-xl tracking-tight uppercase font-mono">
+          <Link to="/">Will Preble</Link>
+        </span>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded border-teal-400 hover:text-white hover:border-white" type="submit">
-          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div className="hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <button type="submit" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold mr-4">
-            Projects
-          </button>
-          <button type="submit" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold mr-4">
-            Info
-          </button>
-          <div className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold">
+      <div className="hidden w-full flex-grow md:flex md:items-center md:w-auto">
+        <div className="text-sm md:flex-grow">
+          <Link to="/projects">
+            <button
+              type="submit"
+              className="block mt-4 md:inline-block md:mt-0 hover:font-bold mr-4"
+            >
+              Projects
+            </button>
+          </Link>
+          <Link to="/info">
+            <button
+              type="submit"
+              className="block mt-4 md:inline-block md:mt-0 hover:font-bold mr-4"
+            >
+              Info
+            </button>
+          </Link>
+          <div className="block mt-4 md:inline-block md:mt-0 hover:font-bold">
             <a href={`${process.env.PUBLIC_URL}/resume.pdf`}>Resume</a>
           </div>
+        </div>
+      </div>
+      <div className="block mx-6">
+        <a
+          className="inline-block pl-2 items-center"
+          href="https://www.linkedin.com/in/will-preble/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn />
+        </a>
+        <a
+          className="inline-block pl-2"
+          href="https://dev.to/wpreble1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <DevTo />
+        </a>
+        <a
+          className="inline-block pl-2"
+          href="https://www.codewars.com/users/wpreble1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CodeWars />
+        </a>
+        <a
+          className="inline-block pl-2"
+          href="https://github.com/wpreble1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+      </div>
+      <div className="inline-block px-8 md:hidden">
+        <div
+          className=""
+          role="button"
+          onClick={toggleSlideDrawer}
+          onKeyDown={toggleSlideDrawer}
+          aria-label="toggle menu"
+          tabIndex={0}
+        >
+          <svg
+            viewBox="0 0 30 30"
+            width="30"
+            height="24"
+            aria-labelledby="Sidebar Menu"
+          >
+            <rect width="30" height="2" rx="3" />
+            <rect y="8" width="30" height="2" rx="3" />
+            <rect y="16" width="30" height="2" rx="3" />
+          </svg>
         </div>
       </div>
     </nav>
