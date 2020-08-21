e7801ddf80e40443734d5308ea8e609e3206ece8
Will Preble <wpreble1@gmail.com>
2020-08-20 19:09:06 -0700

    Rename icon to tile

--- client/src/components/ProjectList.tsx
+++ client/src/components/ProjectList.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
 import { v4 as uuid } from 'uuid';
 
-import projects from '../projectData';
-import ProjectIcon from './ProjectIcon';
+import { projects, displayOrder } from '../projectData';
+import ProjectIcon from './ProjectTile';
 
 interface ProjectListProps {
 }
@@ -10,9 +10,9 @@ interface ProjectListProps {
 const ProjectList: FC<ProjectListProps> = () => {
   return (
     <div className="flex flex-wrap my-4 justify-center mx-10">
-      {projects.map(project => {
+      {displayOrder.map(project => {
         return (
-          <ProjectIcon project={project} key={uuid()} />
+          <ProjectIcon project={projects[project]} key={uuid()} />
         );
       })}
     </div>
