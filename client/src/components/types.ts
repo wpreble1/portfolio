6d82f35ca29bdfa50f55b1dad3410f4c3a526241
Will Preble <wpreble1@gmail.com>
2020-08-20 19:09:16 -0700

    Update project type

--- client/src/components/types.ts
+++ client/src/components/types.ts
@@ -1,9 +1,21 @@
 export interface Project {
+  collaborators?: {[key: string]: string} | null;
+  content: Section[];
+  link: string;
+  press?: {[key: string]: string} | null;
+  tile: Tile;
   title: string;
-  src: string;
-  alt: string;
   url: string;
-  photoCredit?: string;
-  images: string[];
-  text: string[];
+}
+
+interface Section {
+  alt?: string | null;
+  imageSrc?: string | null;
+  imageCredit?: string | null;
+  text?: string | null;
+}
+
+interface Tile {
+  alt: string;
+  src: string;
 }
