dda97369fba1d99dc187880b5d73693663dbe0db
Will Preble <wpreble1@gmail.com>
2020-08-20 19:09:39 -0700

    Add components to project page

--- client/src/pages/ProjectPage.tsx
+++ client/src/pages/ProjectPage.tsx
@@ -2,19 +2,76 @@ import React, { FC, ReactElement } from 'react';
 
 import { Project } from '../components/types';
 
+import { LinkButton, LinkText } from '../components';
+
 interface ProjectPageProps {
   project: Project;
 }
 
-const ProjectPage: FC<ProjectPageProps> = ({
-  project,
-}): ReactElement => {
-  const { title, images, text } = project;
-  
+const ProjectPage: FC<ProjectPageProps> = ({ project }): ReactElement => {
+  const { content, title } = project;
+
   return (
-    <div className="">
-      <h1>{title}</h1>
-      <img src={images[0]}></img>
+    <div 
+      className="p-8 mx-auto"
+      style={{ maxWidth: '900px' }}
+    >
+      {/* Project Title */}
+      <h1 className="uppercase text-xl pb-4 font-mono">{title}</h1>
+
+      {/* Link Button */}
+      {project.link && <LinkButton link={project.link} />}
+
+      {/* Content */}
+      {content.map((section) => {
+        return (
+          <div>
+            {section.text && <p>{section.text}</p>}
+            {section.imageSrc && (
+              <img
+                className="mx-auto"
+                style={{ maxWidth: '750px', minWidth: '300px' }}
+                src={section.imageSrc as string}
+                alt={section.alt as string}
+              />
+            )}
+          </div>
+        );
+      })}
+
+      {/* Collaborator Links */}
+      {project.collaborators && (
+        <div>
+          <h2 className="uppercase">Collaborators</h2>
+          {Object.keys(project.collaborators).map((name) => {
+            return (
+              <LinkText
+                link={
+                  (project.collaborators as { [key: string]: string })[name]
+                }
+                text={name}
+              />
+            );
+          })}
+        </div>
+      )}
+
+      {/* Press Links */}
+      {project.press && (
+        <div>
+          <h2 className="uppercase py-2">Press</h2>
+          <ul>
+            {Object.keys(project.press).map(story => {
+              return (
+                <LinkText
+                  link={(project.press as { [key: string]: string })[story]}
+                  text={story}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
