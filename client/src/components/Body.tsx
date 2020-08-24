23d04f064838b6879243cf811d10a6dc1a2d0407
Will Preble <wpreble1@gmail.com>
2020-08-23 20:57:15 -0700

    Change routes to accept props

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -1,42 +1,51 @@
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, ReactElement } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { projects } from '../projectData';
 
 import {
-  // AbitaSpringsHotel,
-  Crawl,
-  EscapeExtinction,
   HomePage,
   InfoPage,
-  UpClose,
-  OrganizePower,
-  ArthurAshe,
-  TimesSquare,
   ProjectPage,
 } from '../pages';
 
 const Body: FC = (): ReactElement => {
   return (
-    <div className="section">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/projects" component={HomePage} />
-        <Route path="/info" component={InfoPage} />
-        {/* Project Pages */}
-        <Route
-          path="/abita-springs-hotel"
-          render={(props) => <ProjectPage project={projects.ash} />}
-        />
-        <Route path="/arthur-ashe" component={ArthurAshe} />
-        <Route path="/crawl" component={Crawl} />
-        <Route path="/escape-extinction" component={EscapeExtinction} />
-        <Route path="/organize-power" component={OrganizePower} />
-        <Route path="/times-square" component={TimesSquare} />
-        <Route path="/up-close" component={UpClose} />
-      </Switch>
-    </div>
-  )
-}
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/projects" component={HomePage} />
+      <Route path="/info" component={InfoPage} />
+      {/* Project Pages */}
+      <Route
+        path="/abita-springs-hotel"
+        render={(props) => <ProjectPage project={projects.ash} />}
+      />
+      <Route
+        path="/arthur-ashe"
+        render={(props) => <ProjectPage project={projects.aas} />}
+      />
+      <Route
+        path="/crawl"
+        render={(props) => <ProjectPage project={projects.crawl} />}
+      />
+      <Route
+        path="/escape-extinction"
+        render={(props) => <ProjectPage project={projects.ees} />}
+      />
+      <Route
+        path="/organize-power"
+        render={(props) => <ProjectPage project={projects.op} />}
+      />
+      <Route
+        path="/times-square"
+        render={(props) => <ProjectPage project={projects.fts} />}
+      />
+      <Route
+        path="/up-close"
+        render={(props) => <ProjectPage project={projects.uc} />}
+      />
+    </Switch>
+  );
+};
 
 export default Body;
