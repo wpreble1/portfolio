851e25268c3b9f877246cf8d2c023688de5dc5e1
Will Preble <wpreble1@gmail.com>
2020-08-23 20:59:10 -0700

    Style project list

--- client/src/components/ProjectList.tsx
+++ client/src/components/ProjectList.tsx
@@ -9,7 +9,7 @@ interface ProjectListProps {
 
 const ProjectList: FC<ProjectListProps> = () => {
   return (
-    <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap justify-center md:-mx-8">
       {displayOrder.map(project => {
         return (
           <ProjectTile project={projects[project]} key={uuid()} />
