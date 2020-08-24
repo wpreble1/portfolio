adbb9713682686a85ea918167eafba0368f30b55
Will Preble <wpreble1@gmail.com>
2020-08-23 20:57:49 -0700

    Style desktop header

--- client/src/components/HeaderXL.tsx
+++ client/src/components/HeaderXL.tsx
@@ -4,16 +4,13 @@ import { Link } from 'react-router-dom';
 import useDocumentScrollThrottled from './useDocumentScrollThrottled';
 
 import { ScrollData } from './types';
-// import {
-//   LinkedIn, Github, DevTo, CodeWars,
-// } from '../logos';
+import { LinkedIn, Github, DevTo, CodeWars } from '../logos';
 
-interface HeaderXLProps {
-}
+interface HeaderXLProps {}
 
 const HeaderXL: FC<HeaderXLProps> = (): ReactElement => {
   const [hideHeader, setHideHeader] = useState(false);
-  const [showShadow, setShowShadow] = useState(false);
+  const [, setShowShadow] = useState(false);
 
   const MINIMUM_SCROLL = 80;
   const TIMEOUT_DELAY = 0;
@@ -29,16 +26,70 @@ const HeaderXL: FC<HeaderXLProps> = (): ReactElement => {
     }, TIMEOUT_DELAY);
   });
 
-  // const shadowStyle = showShadow ? 'shadowXL' : '';
-  const hiddenStyle = hideHeader ? 'hiddenXL' : '';
+  const hiddenStyle = hideHeader ? 'fade-out' : '';
 
   return (
-    <header className={`hidden xl:block headerXL p-16 ${hiddenStyle}`}>
+    <header className={`hidden lg:block headerXL p-12 ${hiddenStyle}`}>
       <ul className="block text-lg">
-        <li className="py-1">Projects</li>
-        <li className="py-1">Info</li>
-        <li className="py-1">Resume</li>
+        <li className="py-1">
+          <Link className="left" to="/projects">
+            Projects
+          </Link>
+        </li>
+        <li className="py-1">
+          <Link className="left" to="/info">
+            Info
+          </Link>
+        </li>
+        <li className="py-1">
+          <a
+            className="left"
+            href={`${process.env.PUBLIC_URL}/resume.pdf`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Resume
+          </a>
+        </li>
       </ul>
+      <div className="block logosXL mt-24">
+        <a
+          className="pl-2 items-center"
+          href="https://www.linkedin.com/in/will-preble/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn />
+        </a>
+        <a
+          className="pl-2"
+          href="https://github.com/wpreble1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+        <a
+          className="pl-2"
+          href="https://dev.to/wpreble1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <DevTo />
+        </a>
+        <a
+          className="pl-2"
+          href="https://www.codewars.com/users/wpreble1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CodeWars />
+        </a>
+      </div>
+
+      <Link to="/">
+        <div className="logoXL text-lg tracking-widest">WP</div>
+      </Link>
     </header>
   );
 };
