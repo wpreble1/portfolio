e2ed4ee366bf765afee8bef46ebaae7697449960
Will Preble <wpreble1@gmail.com>
2020-08-24 19:50:38 -0700

    Start tech stack feature

---
+++ client/src/components/TechStack.tsx
@@ -0,0 +1,14 @@
+import React, { FC, ReactElement } from 'react';
+
+interface TechStackProps {
+  techs: string[];
+}
+
+const TechStack: FC<TechStackProps> = ({ techs }): ReactElement => {
+  return (
+    <div className="">
+    </div>
+  );
+};
+
+export default TechStack;
