a335ada8f16fe4073cc59a59f41398b70eaa2680
Will Preble <wpreble1@gmail.com>
2020-08-24 19:50:27 -0700

    Add cover photo capability

--- client/src/pages/ProjectPage.tsx
+++ client/src/pages/ProjectPage.tsx
@@ -1,93 +1,108 @@
 import React, { FC, ReactElement } from 'react';
-import { Link } from 'react-router-dom';
 
 import { Project } from '../components/types';
 
-import { LinkButton, LinkText } from '../components';
+import { LinkButton, LinkText, Slideshow } from '../components';
 
 interface ProjectPageProps {
   project: Project;
 }
 
 const ProjectPage: FC<ProjectPageProps> = ({ project }): ReactElement => {
-  const { content, title } = project;
+  const { content, cover, title } = project;
 
   return (
-    <div
-      className="mx-auto section lg:p-0"
-    >
-      {/* Content */}
-      {content.map((section, index) => {
-        if (index === 0) {
-          return (
-            <div className="">
-              <img
-                className="mx-auto w-full object-cover"
-                style={{ minWidth: '300px', height: '100vh' }}
-                src={section.imageSrc as string}
-                alt={section.alt as string}
-              />
-              <div className="wrapper">
-                <div className="flex justify-between items-baseline">
-                  <h1 className="uppercase text-xl font-sans pt-4 inline-block">{title}</h1>
-                  {/* Link Button */}
-                  <div className="inline-block">
-                    {project.link && <LinkButton link={project.link} />}
-                  </div>
-                </div>
-                {section.text && <p className="block">{section.text}</p>}
+    <div className="mx-auto pt-16 px-6 pb-6 lg:p-0">
+      {/* Full bleed cover photo */}
+      {cover.fullscreen && (
+      <div>
+        <img
+          className="mx-auto w-full object-cover"
+          style={{ minWidth: '300px', height: '100vh' }}
+          src={cover.imageSrc as string}
+          alt={cover.alt as string}
+        />
+      </div>
+      )}
+      <div className="h-full lg:mx-40">
+        <div className="lg:mx-12 lg:pt-24">
+          {/* Standard cover photo */}
+          {!cover.fullscreen && typeof cover.imageSrc === 'string' && (
+            <img
+              className="mx-auto"
+              style={{ minWidth: '300px' }}
+              src={cover.imageSrc as string}
+              alt={cover.alt as string}
+            />
+          )}
+          {/* Cover photo slideshow */}
+          {!cover.fullscreen && Array.isArray(cover.imageSrc) && (
+            <Slideshow images={cover.imageSrc} alt={cover.alt} />
+          )}
+
+          {/* Title and Link */}
+          <div className="flex justify-between items-baseline">
+            <h1 className="uppercase text-xl font-sans pt-4 inline-block">{title}</h1>
+            <div className="inline-block">
+              <div className="inline-block mr-4">
+                {project.link && <LinkButton type="link" link={project.link} />}
+              </div>
+              <div className="inline-block">
+                {project.github && <LinkButton type="github" link={project.github} />}
               </div>
             </div>
-          );
-        }
-        return (
-          <div className="wrapper">
-            {section.text && <p>{section.text}</p>}
-            {section.imageSrc && (
-              <img
-                className="mx-auto"
-                style={{ maxWidth: '750px', minWidth: '300px' }}
-                src={section.imageSrc as string}
-                alt={section.alt as string}
-              />
-            )}
           </div>
-        );
-      })}
 
-      {/* Collaborator Links */}
-      {project.collaborators && (
-        <div className="wrapper">
-          <h2 className="uppercase">Collaborators</h2>
-          {Object.keys(project.collaborators).map((name) => {
-            return (
-              <LinkText
-                link={
-                  (project.collaborators as { [key: string]: string })[name]
-                }
-                text={name}
-              />
-            );
-          })}
-        </div>
-      )}
-
-      {/* Press Links */}
-      {project.press && (
-        <div className="wrapper">
-          <h2 className="uppercase py-2">Press</h2>
-          <ul>
-            {Object.keys(project.press).map(story => {
-              return (
-                <LinkText
-                  link={(project.press as { [key: string]: string })[story]}
-                  text={story}
+          {/* Content - Images and Text */}
+          {content.map((section) => (
+            <div>
+              {section.text && <p>{section.text}</p>}
+              {section.imageSrc && (
+                <img
+                  className="mx-auto"
+                  style={{ maxWidth: '750px', minWidth: '300px' }}
+                  src={section.imageSrc as string}
+                  alt={section.alt as string}
                 />
-              );
-            })}
-          </ul>
+              )}
+            </div>
+          ))}
+
+          {/* Collaborator Links */}
+          {project.collaborators && (
+            <div className="pt-4">
+              <h2 className="uppercase">Collaborators</h2>
+              {Object.keys(project.collaborators).map((name) => {
+                return (
+                  <LinkText
+                    link={
+                      (project.collaborators as { [key: string]: string })[name]
+                    }
+                    text={name}
+                  />
+                );
+              })}
+            </div>
+          )}
+
+          {/* Press Links */}
+          {project.press && (
+            <div className="pt-4">
+              <h2 className="uppercase py-2">Press</h2>
+              <ul>
+                {Object.keys(project.press).map(story => {
+                  return (
+                    <LinkText
+                      link={(project.press as { [key: string]: string })[story]}
+                      text={story}
+                    />
+                  );
+                })}
+              </ul>
+            </div>
+          )}
         </div>
-      )}
+      </div>
       {/* <div className="footer-feature">
         <p className="text-xs left-align">Featured Project</p>
         <Link to="/up-close"><p className="text-xl left-align">Up Close</p></Link>
