897a36ae43daefc4ac494b8e06bfe1adc0d60140
Will Preble <wpreble1@gmail.com>
2020-08-26 00:52:59 -0700

    Remove unused server routes

--- server/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import resumeRouter from './resume';
-
-const routes = Router();
-
-routes.use('/resume', resumeRouter);
-
-export default routes;
--- server/routes/resume.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const resumeRouter = express.Router();
-
-resumeRouter.get('/', (req, res) => {
-  res.send('reached /resume!');
-});
-
-export default resumeRouter;
