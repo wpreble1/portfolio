35778fbbbab57b05ecf8a9995eff5ecbca79cfa1
Will Preble <wpreble1@gmail.com>
2020-08-26 00:53:19 -0700

    Remove routes

--- server/server.ts
+++ server/server.ts
@@ -4,8 +4,6 @@ import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import routes from './routes';
-
 dotenv.config();
 
 const app = express();
@@ -15,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/', routes);
 app.use(express.static(`${path.resolve('./')}/client/build`));
 app.get('*', (req: Request, res: Response) => {
   res.sendFile(`${path.resolve('./')}/client/build/index.html`);
