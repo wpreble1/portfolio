66169032c03743284e65045deae03de2e76acc74
Will Preble <wpreble1@gmail.com>
2020-08-26 00:08:04 -0700

    Style info page

--- client/src/pages/InfoPage.tsx
+++ client/src/pages/InfoPage.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactElement } from 'react';
 
 import { LinkText } from '../components';
 
-import test from '../images/uc/uc-tile.jpg';
+import info from '../images/info.jpg';
 
 interface InfoPageProps {
   setFullCover: any;
@@ -14,13 +14,13 @@ const InfoPage: FC<InfoPageProps> = ({ setFullCover }): ReactElement => {
   return (
     <div className="h-full lg:mx-40 pb-8">
       <div className="h-full bg-white my-0 mx-auto pt-16 px-6 pb-6 lg:pt-24 lg:px-12">
-        <img className="pb-8" src={test} alt="" />
         <h2 className="uppercase">Bio</h2>
         <p className="pt-4">
           I&apos;m a full-stack developer currently looking for work in Los
           Angeles.
         </p>
-        <h2 className="uppercase pt-8 pb-1">Contact</h2>
+        <img className="p-8" src={info} alt="" />
+        <h2 className="uppercase pb-1">Contact</h2>
         <ul className="list-none">
           <LinkText
             link="mailto:wpreble1@gmail.com"
