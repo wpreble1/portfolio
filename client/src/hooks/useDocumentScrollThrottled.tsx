9599837bb0779f453728a389eba785c9adc43b5b
Will Preble <wpreble1@gmail.com>
2020-08-25 23:01:17 -0700

    Create hooks folder

--- client/src/hooks/useDocumentScrollThrottled.tsx
+++ client/src/hooks/useDocumentScrollThrottled.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { throttle } from 'lodash';
 
-import { ScrollCallback } from './types';
+import { ScrollCallback } from '../components/types';
 
 const useDocumentScrollThrottled = (callback: ScrollCallback) => {
   const [, setScrollPosition] = useState(0);
