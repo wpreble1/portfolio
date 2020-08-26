743a5516809b70ee08422d0d2e5785ea3fc77341
Will Preble <wpreble1@gmail.com>
2020-08-26 11:39:36 -0700

    Fix margins on mobile header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -32,9 +32,9 @@ const Header: FC<HeaderProps> = (): ReactElement => {
 
   return (
     <header className={`headerMobile fixed top-0 left-0 flex pt-4 justify-between w-full h-12 bg-white ${shadowStyle} ${hiddenStyle} lg:hidden`}>
-      <div className="inline-block text-lg mx-6 hover:text-pink font-serif"><Link className="" to="/">WP</Link></div>
+      <div className="inline-block text-lg ml-6 hover:text-pink font-serif"><Link className="" to="/">WP</Link></div>
       <LogoLinks />
-      <ul className="p-0 mx-6">
+      <ul className="p-0 mr-6">
         <li className="inline-block mx-1"><Link className="left-underline" to="/projects">Projects</Link></li>
         <li className="inline-block mx-1"><Link className="left-underline" to="/info">Info</Link></li>
         <li className="inline-block mx-1"><Link className="left-underline" to="/resume">Resume</Link></li>
