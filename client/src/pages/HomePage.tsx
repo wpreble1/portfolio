ed456ac6f33c95a55bda0cf0715fe4f6aeb132d0
Will Preble <wpreble1@gmail.com>
2020-08-25 23:07:26 -0700

    Reset xl header to black

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect } from 'react';
 
 import ProjectList from '../components/ProjectList';
 
-const HomePage: FC = (): ReactElement => {
+interface HomePageProps {
+  setFullCover: any;
+}
+
+const HomePage: FC<HomePageProps> = ({ setFullCover }): ReactElement => {
+  useEffect(() => {
+    setFullCover(false);
+  }, []);
+
   return (
     <div className="h-full lg:mx-40">
       <div className="h-full bg-white my-0 mx-auto pt-16 px-6 pb-6 lg:pt-12">
