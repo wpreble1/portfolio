39bb7bf4288859b1be2fd993c25568610eff5573
Will Preble <wpreble1@gmail.com>
2020-08-28 18:46:33 -0700

    Update resume

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5cf431cb9207b1572ac31caa18776cb.js"
+  "/precache-manifest.f8adb8efff4aa34819421cee9b73477a.js"
 );
 
 self.addEventListener('message', (event) => {
