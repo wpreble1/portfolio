d02f6d79b00232525e910566b38eab29ffb3a24b
Will Preble <wpreble1@gmail.com>
2020-09-01 16:45:10 -0700

    Start footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactElement, useState } from 'react';
 // import { Route, Switch } from 'react-router-dom';
 
 import { Body, Header, HeaderXL, HeaderXL2 } from './components';
+// import Footer from './components/Footer';
 
 const App: FC = (): ReactElement => {
   const [fullCover, setFullCover] = useState(false);
@@ -11,6 +12,7 @@ const App: FC = (): ReactElement => {
       <Header />
       {fullCover ? <HeaderXL fullCover={fullCover} /> : <HeaderXL2 fullCover={fullCover} />}
       <Body setFullCover={setFullCover} />
+      {/* <Footer /> */}
     </div>
   );
 };
